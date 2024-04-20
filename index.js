9e6a08b3dbed58d2c8f0d701da3db018e9c47392
Srujan Kinjawadekar <64701567+srujank1995@users.noreply.github.com>
2024-04-20 17:26:54 +0530

    Update and rename index.js to file

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
-
-app.listen(port, () => console.log(`This is the beginning of the Node File Upload App`))
